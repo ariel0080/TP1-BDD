f8cdf29e6bad9177b933d309844e35b9bdf4f5d2
ariel0080 <ariel0080@gmail.com>
2019-11-16 15:18:17 -0300

    todo listo para el tp

--- TP1-BDD-BRINO/src/app/app.module.ts
+++ TP1-BDD-BRINO/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,6 +13,7 @@ import {enviroment} from 'src/environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(enviroment.firebaseConfig)
   ],
