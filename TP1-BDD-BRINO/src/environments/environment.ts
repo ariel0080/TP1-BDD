61c1042ef9a90ffb248d349a69fe8eb8269bdf82
ariel0080 <ariel0080@gmail.com>
2019-11-16 15:07:51 -0300

    primer commit

---
+++ TP1-BDD-BRINO/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig:  {
+    apiKey: "AIzaSyANa-Re7JgFEKsoCLKGCaSkP9sehIc78DM",
+    authDomain: "tp-bdd-brino.firebaseapp.com",
+    databaseURL: "https://tp-bdd-brino.firebaseio.com",
+    projectId: "tp-bdd-brino",
+    storageBucket: "tp-bdd-brino.appspot.com",
+    messagingSenderId: "903992996908",
+    appId: "1:903992996908:web:8d76513590aa8185224d89"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
